beab11ce1265dc7a5082155625ef2a5732288945
mmmarsch <meganmarschall@Megans-Mac-Pro.local>
2017-01-31 08:10:38 -0800

    Adding if statement

--- studio2/script.js
+++ studio2/script.js
@@ -13,9 +13,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
         //storing variables
 
         var userName = document.f.userName.value;
+
         var userNumber = document.f.userNumber.value;
+
         var userAnimal= document.f.userAnimal.value;
+
         var userAnimalTwo= document.f.userAnimalTwo.value;
+
         var userAnimalThree= document.f.userAnimalThree.value;
         var userPlace= document.f.userPlace.value;
 
@@ -29,11 +33,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
         + userAnimal + ', the body of a ' + userAnimalTwo + ', and the tail of a ' + userAnimalThree +
         '! How unique! We are very excited to see what it will turn out looking like as well. You have recieved a very special discount and will only need to pay a small sum of '
         + userNumber + ' dollars (some fees not included) and we will deliver your new pet right to your door in ' + userPlace+ '!');
-        //prevent page from reloading
-        return false;
 
+          // alert message for blank fields
 
+        if (userName == "") {
+             alert("Name must be filled out");
+         }
+
+        if (userAnimal == "") {
+              alert("Animal one must be filled out");
+          }
+
+        if (userAnimalTwo == "") {
+              alert("Animal two must be filled out");
+           }
+
+        if (userAnimalThree == "") {
+              alert("Animal three must be filled out");
+            }
+
+        if (userNumber == "") {
+              alert("Number must be filled out");
+            }
+
+        if (userPlace == "") {
+              alert("Place must be filled out");
+            }
+
+             //prevent page from reloading
+
+          return false;
 
   }
+
+
 
 })

28a11a4fe3634c01201aea469976c7ca05607f26
mmmarsch <meganmarschall@Megans-Mac-Pro.local>
2017-01-31 02:30:40 -0800

    studio 2

--- studio2/script.js
+++ studio2/script.js
@@ -12,20 +12,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         //storing variables
 
-        var userFood = document.f.userFood.value;
+        var userName = document.f.userName.value;
         var userNumber = document.f.userNumber.value;
-        var userEmotion= document.f.userEmotion.value;
-        var userEmotionTwo= document.f.userEmotionTwo.value;
-        var userCook= document.f.userCook.value;
+        var userAnimal= document.f.userAnimal.value;
+        var userAnimalTwo= document.f.userAnimalTwo.value;
+        var userAnimalThree= document.f.userAnimalThree.value;
+        var userPlace= document.f.userPlace.value;
+
 
         // storing myMsg
 
         var myMsg = document.getElementById("myMsg");
 
         //concatenate a message
-        myMsg.innerHTML= ('Hi, ' + userFood + '!' + ' Your favorite color is ' + userNumber +'!');
+        myMsg.innerHTML= ('Congratulations ' + userName + '!' + ' You have started the first step to making your very own Chimera! Your animal will have the head of a  '
+        + userAnimal + ', the body of a ' + userAnimalTwo + ', and the tail of a ' + userAnimalThree +
+        '! How unique! We are very excited to see what it will turn out looking like as well. You have recieved a very special discount and will only need to pay a small sum of '
+        + userNumber + ' dollars (some fees not included) and we will deliver your new pet right to your door in ' + userPlace+ '!');
         //prevent page from reloading
         return false;
+
+
 
   }
 

977cb242a9259e7203b0a1aaaef4f1389e90b97e
mmmarsch <meganmarschall@Megans-Mac-Pro.local>
2017-02-17 00:09:17 -0800

    correct time

--- studio4/script.js
+++ studio4/script.js
@@ -63,14 +63,14 @@ document.addEventListener("DOMContentLoaded", function() {
       warning.style.opacity = 1;
     }
 
-    var countDownDate = new Date("Jan 5, 2018 15:37:25").getTime();
+    var countDownDate = new Date("Jan 20, 2020 15:37:25").getTime();
     var x = setInterval(function() {
     var now = new Date().getTime();
       var distance = countDownDate - now;
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);  
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
       document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
       + minutes + "m " + seconds + "s ";
       if (distance < 0) {

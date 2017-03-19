7af5bca104b7f63e52b1d1eb51a6fc3bef75e3a6
mmmarsch <mmmarschall@ucdavis.edu>
2017-03-19 00:31:05 -0700

    final

---
+++ project/final/stories/storyone/interaction/pgtwo/pgthree/script.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+
+    var audio, playbtn;
+    function initAudioPlayer () {
+      audio = new Audio ();
+      audio.src = "Take5.mp3";
+      audio.loop = true;
+      audio.play ();
+      // object references
+      playbtn= document.getElementById ("playpausebtn");
+      // event handling
+      playbtn.addEventListener ("click", playPause);
+      // function
+      function playPause() {
+        if(audio.paused){
+          audio.play();
+          playbtn.style.background = "url(pause.png) no-repeat";
+        } else {
+          audio.pause();
+          playbtn.style.background= "url(play.png) no-repeat";
+        }
+      }
+    }
+    window.addEventListener("load", initAudioPlayer);
+
+});
--- project/prototypethree/stories/storyone/interaction/script.js
+++ project/prototypethree/stories/storyone/interaction/script.js
@@ -3,6 +3,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
-    // all other js here
+    var audio, playbtn;
+    function initAudioPlayer () {
+      audio = new Audio ();
+      audio.src = "take1.mp3";
+      audio.loop = true;
+      audio.play ();
+      // object references
+      playbtn= document.getElementById ("playpausebtn");
+      // event handling
+      playbtn.addEventListener ("click", playPause);
+      // function
+      function playPause() {
+        if(audio.paused){
+          audio.play();
+          playbtn.style.background = "url(pause.png) no-repeat";
+        } else {
+          audio.pause();
+          playbtn.style.background= "url(play.png) no-repeat";
+        }
+      }
+    }
+    window.addEventListener("load", initAudioPlayer);
 
 });
